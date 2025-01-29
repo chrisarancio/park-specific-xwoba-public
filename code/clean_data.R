35ecbf3aa3d910ecef15109eaba9a03fae8d9bfc
Luke Walsh <lwalsh8@friars.providence.edu>
2025-01-29 14:21:16 -0500

    update data cleaning script

--- code/clean_data.R
+++ code/clean_data.R
@@ -1,9 +1,26 @@
 library(dplyr)
+library(tidyr)
 
 data <- read.csv("raw-data/statcast2024.csv")
 
-filtered_df <- data %>%
-  filter(description == "hit_into_play")
+## add a column for batted balls 'ball_in_play'
+## add a column for total bases
+df <- data %>%
+  mutate(ball_in_play = case_when(
+    description == "hit_into_play" ~ 1,
+    TRUE ~ 0
+  )) %>%
+  mutate(total_bases = case_when(
+    events == 'single' ~ 1,
+    events == 'double' ~ 2,
+    events == 'triple' ~ 3,
+    events == 'home_run' ~ 4,
+    TRUE ~ 0
+  ))
 
-selected_df <- filtered_df %>% 
-  select(game_date, batter, home_team, events, launch_speed, launch_angle)
+## get only batted balls for training
+df_bip <- df %>%
+  filter(description == "hit_into_play") %>%
+  drop_na(launch_speed, launch_angle, ball_in_play) %>%
+  select(game_date, batter, home_team, events, launch_speed, launch_angle, 
+         woba_value, ball_in_play, total_bases)
