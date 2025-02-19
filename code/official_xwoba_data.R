2cd3366e9478dfffd5aab6fee9ccb47a2d9e6953
Luke Walsh <lukewalsh515@gmail.com>
2025-02-19 17:00:13 -0500

    update xwoba data script

--- code/official_xwoba_data.R
+++ code/official_xwoba_data.R
@@ -7,3 +7,5 @@ fg_data <- fg_batter_leaders(startseason = 2024, endseason = 2024)
 # grab season player name and xwOBA columns -- 1454 players
 cleaned_fg_data <- fg_data |>
   select(Season, PlayerName, xwOBA)
+
+saveRDS(cleaned_fg_data, "./data/mlb_xwoba_2024.rds")
