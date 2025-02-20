2fb5ff69f57e9c915a4d982af8afee1eef4412e8
Luke Walsh <lukewalsh515@gmail.com>
2025-02-19 20:57:56 -0500

    update xwoba script

--- code/official_xwoba_data.R
+++ code/official_xwoba_data.R
@@ -4,8 +4,9 @@ library(dplyr)
 # get data from FanGraphs using baseballr function
 fg_data <- fg_batter_leaders(startseason = 2024, endseason = 2024)
 
-# grab season player name and xwOBA columns -- 1454 players
+# grab season player name and xwOBA columns for players with more than 100 PAs
 cleaned_fg_data <- fg_data |>
+  filter(PA > 100) |>
   select(xMLBAMID, PlayerName, xwOBA)
-
+  
 saveRDS(cleaned_fg_data, "./data/mlb_xwoba_2024.rds")
