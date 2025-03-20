c5d1e7161ce2271864289a643838a160dd03bd81
Luke Walsh <lukewalsh515@gmail.com>
2025-03-20 11:43:09 -0400

    get the player team column from fg data

--- code/official_xwoba_data.R
+++ code/official_xwoba_data.R
@@ -7,6 +7,6 @@ fg_data <- fg_batter_leaders(startseason = 2024, endseason = 2024)
 # grab season player name and xwOBA columns for players with more than 100 PAs
 cleaned_fg_data <- fg_data |>
   filter(PA > 100) |>
-  select(xMLBAMID, PlayerName, xwOBA)
+  select(xMLBAMID, team_name, PlayerName, xwOBA)
   
 saveRDS(cleaned_fg_data, "./data/mlb_xwoba_2024.rds")
