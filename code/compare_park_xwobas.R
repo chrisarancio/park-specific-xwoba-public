320c3c545b0d26307a8c49fe129d6678328e2131
Luke Walsh <lukewalsh515@gmail.com>
2025-03-20 11:42:50 -0400

    Update compare_park_xwobas.R

--- code/compare_park_xwobas.R
+++ code/compare_park_xwobas.R
@@ -1,10 +1,11 @@
 library(dplyr)
+library(ggplot2)
 
 #----------- COMPARING TO OFFICIAL MLB VALUES
 # Per Baseball Savant: "* Qualifiers: 2.1 PA per team game for batters, 1.25 PA per team game for pitchers."
 # URL: https://baseballsavant.mlb.com/leaderboard/expected_statistics 
 parks <- list.files("./data/parks/xwoba", full.names = TRUE)
-official_xwOBA_values <- readRDS("./data/mlb_xwoba_2024.rds")
+official_xwOBA_values <- readRDS("./data/mlb_xwoba_2024_with_teams.rds")
 knn_xwOBA_values <- readRDS("./data/knn_xwoba_vs_mlb.rds")
 
 knn_xwOBA_values <- knn_xwOBA_values |>
@@ -21,4 +22,40 @@ for (park in parks) {
     inner_join(park_data, join_by(`player_id` == `batter`))
 }
 
-saveRDS(parks_and_mlb_xwoba, "./data/parks_and_mlb_xwoba.rds")
+#saveRDS(parks_and_mlb_xwoba, "./data/parks_and_mlb_xwoba.rds")
+
+parks <- c("BOS", "BAL", "COL", "HOU", "LAD", "NYM", "NYY", "PHI", "SEA", "SF")
+
+home_diff_df <- data.frame()
+for (park in parks) {
+  home_park_xwoba <- paste0(park, "_xwOBA")
+  filtered <- parks_and_mlb_xwoba |>
+    filter(team_name == park) |>
+    select(player_id, team_name, player_name, official_xwOBA, all_of(home_park_xwoba)) |>
+    rename(home_xwOBA = all_of(home_park_xwoba)) 
+  
+  filtered$home_diff <- filtered$official_xwOBA - filtered$home_xwOBA
+  home_diff_df <- bind_rows(home_diff_df, filtered)
+}
+
+home_diff_df <- home_diff_df |>
+  mutate(
+    value_color = ifelse(home_diff > 0, "Positive", "Negative")
+  )
+
+median_official_xwOBA <- median(home_diff_df$official_xwOBA)
+median_home_xwOBA <- median(home_diff_df$home_xwOBA)
+
+# Create the scatter plot
+ggplot(home_diff_df, aes(x = home_xwOBA, y = official_xwOBA)) +
+  geom_point(aes(color = value_color)) +
+  geom_vline(xintercept = median_official_xwOBA, linetype = "dashed", color = "black") +
+  geom_hline(yintercept = median_home_xwOBA, linetype = "dashed", color = "black") +
+  geom_abline(slope = 1, intercept = 0, linetype = "solid", color = "blue") +
+  labs(
+    title = "Official xwOBA vs Home xwOBA",
+    x = "Official xwOBA",
+    y = "Home xwOBA"  
+    )
+
+
