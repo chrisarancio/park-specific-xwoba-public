c17ea1ba50f6accd5738ad10c59637a54d746cc5
Luke Walsh <lukewalsh515@gmail.com>
2025-04-06 22:03:02 -0400

    filter by reg season games by type not by date

--- code/clean_data_knn.R
+++ code/clean_data_knn.R
@@ -5,7 +5,7 @@ data <- readRDS("./data/statcast2024(R).rds")
 data <- as_tibble(data)
 # Make sure that we are only considering data within the regular season
 data <- data |>
-  filter(game_date >= as.Date("2024-03-28") & game_date <= as.Date("2024-09-30"))
+  filter(game_type == "R")
 
 ## add a column for batted balls 'ball_in_play'
 ## add a column for total bases
