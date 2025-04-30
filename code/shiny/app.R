fbd14f3b09729cecb45bd86a8b33c7178e7ab065
Luke Walsh <lukewalsh515@gmail.com>
2025-04-29 23:18:05 -0400

    update the about section of the dashboard

--- code/shiny/app.R
+++ code/shiny/app.R
@@ -2,9 +2,7 @@ library(tidyverse)
 library(shiny)
 library(dplyr)
 
-project_dir <- rstudioapi::getActiveProject()
-setwd(project_dir)
-data <- readRDS("./data/new_parks/final_parks_and_mlb_xwoba.rds")
+data <- readRDS("final_parks_and_mlb_xwoba.rds")
 
 data <- data |>
   filter(!is.na(value))
@@ -43,9 +41,12 @@ ui <- fluidPage(
                  plotOutput(outputId = "barPlot")
         ),
         tabPanel("About",
+                 h3("Project Overview"),
+                 p("In the world of baseball analytics, there are many methods used to try to represent a player’s offensive ability but this project focuses mostly on the wOBA and xwOBA metrics. Weighted On-base Average (wOBA) is a metric defined by the MLB as “a version of on-base percentage that accounts for how a player reached base -- instead of simply considering whether a player reached base. The value for each method of reaching base is determined by how much that event is worth in relation to projected runs scored.”  Rather than a simple on-base percentage (OBP), which refers to “how frequently a batter reaches base per plate appearance,”  the weighted OBA (wOBA) has the advantage of considering how different events will have different impacts on scoring runs. For example, the 2023 wOBA formula assigns a linear weight of 1.569 to a triple and a linear weight of 0.833 for a single in that season. "),
+                 p("However, there is an even more indicative statistic for a player’s offensive skill called Expected Weighted On-base Average (xwOBA).  xwOBA incorporates “exit velocity, launch angle and, on certain types of batted balls, Sprint Speed” and “allows for the formation of said player's xwOBA based on the quality of contact, instead of the actual outcomes.”  In other words, xwOBA removes defense from the equation and provides probabilities for a single, double, triple, and homerun “based on the results of comparable batted balls since Statcast was implemented Major League wide in 2015.” "),
+                 p("For this project, it is important to note that these factors or weights indicate the “adjusted run expectancy of a batting event in the context of the season as a whole.” This analysis trained a model to predict xwOBA for a player on a per-ballpark basis. The reasoning for this is that the outfield walls of each ballpark can vary greatly. In fact, “Kansas City and Toronto are the only two stadiums in MLB with symmetrical outfield dimensions and uniform wall height.”  This means that scoring a homerun at each park will require different exit velocities and launch angles to make it over the outfield walls. Since xwOBA considers exit velocity and launch angle in its calculations, this analysis considers how different ballparks could affect a player’s xwOBA."),
                  h3("What does this chart mean?"),
-                 p("Something idk"),
-                 p("really cool")
+                 p("Using KNN models trained on pitches from each ballpark and the MLB wOBA weights for each year, we calculated a park-specific xwOBA for each player. We can then compare the average difference between the predicted park xwOBA and the MLB official xwOBA values for every player for the 2015-2024 seasons. These average differences for a certain season and ballpark can then be compared to a specific player's predicted xwOBA during that season and at that ballpark. These differences can highlight certain ballparks where a player may have overperformed or underperformed (compared to the average xwOBA) during a desired season. For example, one can examine which players have overperformed (in terms of xwOBA) at their home ballpark during various seasons.")
         )
       )
     )
@@ -65,7 +66,7 @@ server <- function(input, output, session) {
     years_available <- years_available[!is.na(years_available)]
     
     
-    # Update the year dropdown based on selected player
+    # update the year dropdown based on selected player
     updateSelectInput(session, "year_select", choices = years_available)
   })
   
@@ -83,7 +84,7 @@ server <- function(input, output, session) {
         plot.title = element_text(size = 22, face = "bold", hjust = 0.5),
         axis.title.x = element_text(size = 16),
         axis.title.y = element_text(size = 16),
-        axis.text.x = element_text(size = 12),  # Increase x-axis tick label size
+        axis.text.x = element_text(size = 12), 
         axis.text.y = element_text(size = 12), 
         legend.position = "bottom",
         legend.text = element_text(size = 14),
